1882789f79d5cf3d4294bc5a2d3c40084c27e6ed
Unity <Unity@rbiu.local>
2026-02-26 12:59:58 +0500

    udpate

--- resources/views/layouts/avto.blade.php
+++ resources/views/layouts/avto.blade.php
@@ -13,44 +13,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">   
-        <header>
-            <div class="header">
-                
-                <div class="">
-                    <p>Ваш город:
-                        <select name="" id="">
-                            <option value="Челябинск">Челябинск</option>
-                            <option value="Екатеренбург">Екатеренбург</option>
-                            <option value="Москва">Москва</option>
-                            <option value="Санкт-Петербург">Санкт-Петербург</option>
-                            <option value="Саратов">Саратов</option>
-                        </select></p>
-                    </div>
-                    <div class="header-menu">
-                        <a href="">Акции</a>
-                        <a href="">О компании</a>
-                        <a href="">Адреса</a>
-                        <a href="">Контакты</a>
-                    </div>
-                    <p class="">8 (900) 000 00 00</p>
-            </div>
-            <div class="line"></div>
-            <div class="header">
-                <img src="./img/logo.svg" alt="">
-                 <div class="flex gap-2">
-                    <button class="but_color px-2.5 py-2 flex items-center gap-1"><img src="./img/burg_menu.svg" alt="">Каталог</button>
-                    <div class="search">
-                        <div class="flex items-center">
-                            <input class="img_search rounded-lg inp_sear" type="text" placeholder="Поиск">
-                        </div>
-                    </div>
-                 </div>
-                 <div class="flex items-center gap-2">
-                    <button class=""><img src="./img/basket.svg" alt=""></button>
-                    <button><img src="./img/profile.svg" alt=""></button>
-                 </div>
-            </div>
-        </header>
+        <x-header></x-header>
         <main>
             <div class="block_1">
                 <p class="title mb-5">Запчасти <br>для коммерческой <br>техники и спецтехники</p>
@@ -187,6 +150,11 @@
                         </div>
                         <button class="but_color px-[12px] py-[8px]">Купить</button>
                     </div>
+                </div>
+            </div>
+            <div class="block_5">
+                <div class="form">
+
                 </div>
             </div>
         </main>
